3cffe289134642def7617a2f81c15b743367db5a
박경태 <gyeongtae.park@catenoid.net>
2022-05-18 21:49:49 +0900

    코드수정

--- 2_spark-cluster-zeppelin/dev/pi.py
+++ 2_spark-cluster-zeppelin/dev/pi.py
@@ -27,9 +27,9 @@ if __name__ == "__main__":
         Usage: pi [partitions]
     """
     spark = SparkSession\
-        .builder\
-        .appName("PythonPi")\
-        .getOrCreate()
+            .builder\
+            .appName("PythonPi")\
+            .getOrCreate()
 
     partitions = int(sys.argv[1]) if len(sys.argv) > 1 else 2
     n = 100000 * partitions

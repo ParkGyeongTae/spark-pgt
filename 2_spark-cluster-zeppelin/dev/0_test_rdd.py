4ba9dcc08ea5841b4884e28acca98b00ca3e97a3
박경태 <gyeongtae.park@catenoid.net>
2022-05-29 21:03:50 +0900

    rdd 코드 수정

--- 2_spark-cluster-zeppelin/dev/0_test_rdd.py
+++ 2_spark-cluster-zeppelin/dev/0_test_rdd.py
@@ -1,71 +1,73 @@
 from pyspark.sql import SparkSession
 
-if __name__ == "__main__":
+spark = SparkSession\
+        .builder\
+        .appName("0_save_file")\
+        .getOrCreate()
 
-    spark = SparkSession\
-            .builder\
-            .appName("0_save_file")\
-            .getOrCreate()
+sc = spark.sparkContext
 
-    sc = spark.sparkContext
+lines = sc.parallelize(['i love you', 'you are my friend', 'my name is park'])
 
-    lines = sc.parallelize(['i love you', 'you are my friend', 'my name is park'])
-    
-    print(f'lines : {lines.collect()}' )
+print(f'lines : {lines.collect()}' )
 
-    # pairs = lines.map(lambda s: s.split(" ")) # 띄어쓰기로 나누기
-    # pairs = pairs.filter(lambda x: len(x) > 3) # 단어의 개수가 3개를 초과하는 것만 필터
-    # pairs = lines.flatMap(lambda x: x.split(" ")).map(lambda word: (word, 1)) # 모든 단어를 map 하기
+# pairs = lines.map(lambda s: s.split(" ")) # 띄어쓰기로 나누기
+# pairs = pairs.filter(lambda x: len(x) > 3) # 단어의 개수가 3개를 초과하는 것만 필터
+# pairs = lines.flatMap(lambda x: x.split(" ")).map(lambda word: (word, 1)) # 모든 단어를 map 하기
 
-    pairs = lines.flatMap(lambda x: x.split(" ")).map(lambda word: (word, 1)).groupByKey().mapValues(sum).sortByKey(True)
+pairs = lines.flatMap(lambda x: x.split(" ")).map(lambda word: (word, 1)).groupByKey().mapValues(sum).sortByKey(True)
 
-    print(f'pairs : {pairs.collect()}' )
+print(f'pairs : {pairs.collect()}' )
 
-    spark.stop()
+spark.stop()
 
 
-    '''
-    spark = SparkSession\
-            .builder\
-            .appName("0_save_file")\
-            .getOrCreate()
-    
-    alphabet_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 
-                    'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+'''
+from pyspark.sql import SparkSession
 
-    alphabet_rdd = spark.sparkContext.parallelize(alphabet_list)
-    number_rdd = spark.sparkContext.parallelize(range(0, 26))
+spark = SparkSession\
+        .builder\
+        .appName("0_save_file")\
+        .getOrCreate()
 
-    # print(alphabet_rdd.collect())
-    # print(alphabet_rdd.count())
-    # print(number_rdd.collect())
-    # print(number_rdd.count())
+alphabet_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 
+                'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-    alphabet_rdd.saveAsTextFile("/home/spark/result/0_save_file")
-    number_rdd.saveAsTextFile("/home/spark/result/0_save_file")
+alphabet_rdd = spark.sparkContext.parallelize(alphabet_list)
+number_rdd = spark.sparkContext.parallelize(range(0, 26))
 
-    spark.stop()
-    '''
+# print(alphabet_rdd.collect())
+# print(alphabet_rdd.count())
+# print(number_rdd.collect())
+# print(number_rdd.count())
 
-    '''
-    spark = SparkSession\
-            .builder\
-            .appName("0_save_file")\
-            .getOrCreate()
+alphabet_rdd.saveAsTextFile("/home/spark/result/0_save_file")
+number_rdd.saveAsTextFile("/home/spark/result/0_save_file")
 
-    sc = spark.sparkContext
+spark.stop()
+'''
 
-    line_1 = sc.parallelize(['0', '1', '2', '3', '4'])
-    line_2 = sc.parallelize(['5', '6', '7', '8', '9'])
-    line_3 = sc.parallelize(['10', '11', '12', '13', '14'])
+'''
+from pyspark.sql import SparkSession
 
-    line_all = line_1.union(line_2).union(line_3)
+spark = SparkSession\
+        .builder\
+        .appName("0_save_file")\
+        .getOrCreate()
 
-    line_filter = line_all.filter(lambda x: "1" in x)
+sc = spark.sparkContext
 
-    print('list_filter 전체 :', f'{line_filter.count()}, {line_filter.collect()}')
-    print('list_filter 2개 :', f'{line_filter.take(2)}')
-    print('list_filter 4개 :', f'{line_filter.take(4)}')
+line_1 = sc.parallelize(['0', '1', '2', '3', '4'])
+line_2 = sc.parallelize(['5', '6', '7', '8', '9'])
+line_3 = sc.parallelize(['10', '11', '12', '13', '14'])
 
-    spark.stop()
-    '''
+line_all = line_1.union(line_2).union(line_3)
+
+line_filter = line_all.filter(lambda x: "1" in x)
+
+print('list_filter 전체 :', f'{line_filter.count()}, {line_filter.collect()}')
+print('list_filter 2개 :', f'{line_filter.take(2)}')
+print('list_filter 4개 :', f'{line_filter.take(4)}')
+
+spark.stop()
+'''

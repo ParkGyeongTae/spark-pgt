5ee663b76593749733f42379af7f64c8ceb51b99
박경태 <gyeongtae.park@catenoid.net>
2022-05-29 21:19:46 +0900

    map과 flatmap 차이 확인하는 테스트 코드

--- 2_spark-cluster-zeppelin/dev/0_test_rdd.py
+++ 2_spark-cluster-zeppelin/dev/0_test_rdd.py
@@ -8,6 +8,33 @@ spark = SparkSession\
 
 sc = spark.sparkContext
 
+line_1 = 'i love you'
+line_2 = 'you are my friend'
+line_3 = 'my name is park'
+
+lines = sc.parallelize([line_1, line_2, line_3])
+
+lines_map = lines.map(lambda x: x.split(' '))
+lines_flatmap = lines.flatMap(lambda x: x.split(' '))
+
+print(f'lines.collect()         : {lines.collect()}')
+print(f'lines_map.collect()     : {lines_map.collect()}')
+print(f'lines_flatmap.collect() : {lines_flatmap.collect()}')
+
+spark.stop()
+
+
+
+'''
+from pyspark.sql import SparkSession
+
+spark = SparkSession\
+        .builder\
+        .appName("0_save_file")\
+        .getOrCreate()
+
+sc = spark.sparkContext
+
 data = sc.parallelize(range(0, 10))
 data_map = data.map(lambda x: x * x)
 
@@ -20,6 +47,8 @@ print(f'data_reduce     : {data_reduce}')
 print(f'data_map_reduce : {data_map_reduce}')
 
 spark.stop()
+'''
+
 
 '''
 from pyspark.sql import SparkSession

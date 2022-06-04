3113ca2eb55292feb53c031f0b29129feaa38d37
박경태 <gyeongtae.park@catenoid.net>
2022-06-04 18:23:21 +0900

    자동으로 pyspark 설치하도록 수정

--- 2_spark-cluster-zeppelin/dev/2_test_sparksession.py
+++ 2_spark-cluster-zeppelin/dev/2_test_sparksession.py
@@ -1,4 +1,6 @@
 from pyspark.sql import SparkSession
+from pyspark import SparkConf
+from pyspark import SparkContext
 
 spark = SparkSession \
         .builder \
@@ -8,6 +10,22 @@ spark = SparkSession \
 
 sc = spark.sparkContext
 
-print(sc.getConf().getAll())
+print(SparkConf())
+exit()
+
+for i in sc.getConf().getAll():
+    print(i)
 
 spark.stop()
+
+from pyspark import SparkConf
+from pyspark import SparkContext
+# from pyspark.sql.functions import lit
+# from pyspark.sql.types import DoubleType
+
+# sc.stop()
+# conf = SparkConf().setAppName("comstat-test").set("spark.yarn.driver.memoryOverhead", "2048")\
+# .set("spark.yarn.executor.memoryOverhead", "2048")\
+# .set("spark.default.parallelism", "116")\
+# .set("spark.shuffle.compress", "true")\
+# .set("spark.io.compression.codec", "snappy")

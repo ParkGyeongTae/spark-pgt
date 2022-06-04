bc352ea8152442c4cc4e04e9f381e740b268a0d1
박경태 <gyeongtae.park@catenoid.net>
2022-06-04 19:43:18 +0900

    필요없는 코드 제거

--- 2_spark-cluster-zeppelin/dev/2_test_sparkconf.py
+++ 2_spark-cluster-zeppelin/dev/2_test_sparkconf.py
@@ -1,3 +1,4 @@
+
 from pyspark.conf import SparkConf
 from pyspark.context import SparkContext
 
@@ -17,99 +18,53 @@ for setting in sc._conf.getAll():
 sc.stop()
 
 
-# sc = spark.sparkConf()
-# spark.conf.set('spark.executor.memory', '1g')
+'''
+from pyspark.conf import SparkConf
+from pyspark.context import SparkContext
 
-# spark.conf.get('spark.executor.memory')
-# for i in sc._conf.getAll():
-#     print(i)
+conf = SparkConf().setAll([('spark.app.name', '2_test_sparkconf'),
+                           ('spark.master', 'spark://spark-master:17077'),
+                           ('spark.driver.cores', '1'),
+                           ('spark.driver.memory','1g'),
+                           ('spark.executor.memory', '1g'),
+                           ('spark.executor.cores', '2'),
+                           ('spark.cores.max', '2')])
 
+sc = SparkContext(conf = conf)
 
+for setting in sc._conf.getAll():
+    print(setting)
 
-# conf = SparkConf()
+sc.stop()
+'''
 
-# conf.setMaster('spark://spark-master:17077') \
-#     .setAppName('2_test_sparksession') \
-#     .set
+'''
+from pyspark.conf import SparkConf
+from pyspark.context import SparkContext
 
-# sc = SparkContext(conf = conf)
-# sqlContext = SQLContext(sc)
+conf = SparkConf().setAll([('spark.app.name', '2_test_sparkconf'),
+                           ('spark.master', 'spark://spark-master:17077')])
+sc = SparkContext(conf = conf)
 
-# print('spark.master :',   conf.get('spark.master'))
-# print('spark.app.name :', conf.get('spark.app.name'))
-# print('spark.home :',     conf.get('spark.home'))
-# print('spark.home :',     conf.get('spark.driver.memory'))
+print('first')
+for setting in sc._conf.getAll():
+    print(setting)
 
+sc.stop()
 
+conf = SparkConf().setAll([('spark.app.name', '2_test_sparkconf'),
+                           ('spark.master', 'spark://spark-master:17077'),
+                           ('spark.driver.cores', '1'),
+                           ('spark.driver.memory','1g'),
+                           ('spark.executor.memory', '1g'),
+                           ('spark.executor.cores', '2'),
+                           ('spark.cores.max', '2')])
 
+sc = SparkContext(conf = conf)
 
-# print(sc._conf.getAll())
+print('second')
+for setting in sc._conf.getAll():
+    print(setting)
 
-
-# sc = SparkContext(conf=conf)
-# sc.master
-# 'local'
-# sc.appName
-# 'My app'
-# sc.sparkHome is None
-# True
-
-# from pyspark.conf import SparkConf
-# from pyspark.context import SparkContext
-# conf = SparkConf()
-# conf.setMaster("local").setAppName("My app")
-# <pyspark.conf.SparkConf object at ...>
-# conf.get("spark.master")
-
-
-
-# for i in sc.getConf().getAll():
-#     print(i)
-
-# spark.stop()
-
-# from pyspark import SparkConf
-# from pyspark import SparkContext
-# # from pyspark.sql.functions import lit
-# from pyspark.sql.types import DoubleType
-
-# sc.stop()
-# conf = SparkConf().setAppName("comstat-test").set("spark.yarn.driver.memoryOverhead", "2048")\
-# .set("spark.yarn.executor.memoryOverhead", "2048")\
-# .set("spark.default.parallelism", "116")\
-# .set("spark.shuffle.compress", "true")\
-# .set("spark.io.compression.codec", "snappy")
-
-
-# from pyspark.conf import SparkConf
-# from pyspark.context import SparkContext
-# from pyspark.sql import SQLContext, SparkSession
-
-# spark = SparkSession.builder \
-#                     .appName('2_test_sparksession') \
-#                     .master('spark://spark-master:17077') \
-#                     .getOrCreate()
-
-# conf = SparkConf()
-
-# conf.setMaster('spark://spark-master:17077') \
-#     .setAppName('2_test_sparksession') \
-#     .set
-
-# sc = SparkContext(conf = conf)
-# sqlContext = SQLContext(sc)
-
-# # print('spark.master :',   conf.get('spark.master'))
-# # print('spark.app.name :', conf.get('spark.app.name'))
-# # print('spark.home :',     conf.get('spark.home'))
-# # print('spark.home :',     conf.get('spark.driver.memory'))
-
-
-
-
-# # print(sc._conf.getAll())
-
-# for i in sc._conf.getAll():
-#     print(i)
-
-# sc.stop()
+sc.stop()
+'''
--- 2_spark-cluster-zeppelin/dev/3_test_sparksession.py
+++ 2_spark-cluster-zeppelin/dev/3_test_sparksession.py
@@ -12,121 +12,18 @@ for setting in sc._conf.getAll():
 
 sc.stop()
 
+'''
+from pyspark.sql import SparkSession
 
+spark = SparkSession.builder \
+                    .appName('3_test_sparksession') \
+                    .master('spark://spark-master:17077') \
+                    .getOrCreate()
 
+sc = spark.sparkContext
 
+for setting in sc._conf.getAll():
+    print(setting)
 
-
-
-# from pyspark.conf import SparkConf
-# from pyspark.context import SparkContext
-
-# conf = SparkConf().setAll([('spark.app.name', '2_test_sparkconf'),
-#                            ('spark.master', 'spark://spark-master:17077'),
-#                            ('spark.driver.cores', '1'),
-#                            ('spark.driver.memory','1g'),
-#                            ('spark.executor.memory', '1g'),
-#                            ('spark.executor.cores', '2'),
-#                            ('spark.cores.max', '2')])
-
-# sc = SparkContext(conf = conf)
-
-
-
-
-# sc = spark.sparkConf()
-# spark.conf.set('spark.executor.memory', '1g')
-
-# spark.conf.get('spark.executor.memory')
-# for i in sc._conf.getAll():
-#     print(i)
-
-
-
-# conf = SparkConf()
-
-# conf.setMaster('spark://spark-master:17077') \
-#     .setAppName('2_test_sparksession') \
-#     .set
-
-# sc = SparkContext(conf = conf)
-# sqlContext = SQLContext(sc)
-
-# print('spark.master :',   conf.get('spark.master'))
-# print('spark.app.name :', conf.get('spark.app.name'))
-# print('spark.home :',     conf.get('spark.home'))
-# print('spark.home :',     conf.get('spark.driver.memory'))
-
-
-
-
-# print(sc._conf.getAll())
-
-
-# sc = SparkContext(conf=conf)
-# sc.master
-# 'local'
-# sc.appName
-# 'My app'
-# sc.sparkHome is None
-# True
-
-# from pyspark.conf import SparkConf
-# from pyspark.context import SparkContext
-# conf = SparkConf()
-# conf.setMaster("local").setAppName("My app")
-# <pyspark.conf.SparkConf object at ...>
-# conf.get("spark.master")
-
-
-
-# for i in sc.getConf().getAll():
-#     print(i)
-
-# spark.stop()
-
-# from pyspark import SparkConf
-# from pyspark import SparkContext
-# # from pyspark.sql.functions import lit
-# from pyspark.sql.types import DoubleType
-
-# sc.stop()
-# conf = SparkConf().setAppName("comstat-test").set("spark.yarn.driver.memoryOverhead", "2048")\
-# .set("spark.yarn.executor.memoryOverhead", "2048")\
-# .set("spark.default.parallelism", "116")\
-# .set("spark.shuffle.compress", "true")\
-# .set("spark.io.compression.codec", "snappy")
-
-
-# from pyspark.conf import SparkConf
-# from pyspark.context import SparkContext
-# from pyspark.sql import SQLContext, SparkSession
-
-# spark = SparkSession.builder \
-#                     .appName('2_test_sparksession') \
-#                     .master('spark://spark-master:17077') \
-#                     .getOrCreate()
-
-# conf = SparkConf()
-
-# conf.setMaster('spark://spark-master:17077') \
-#     .setAppName('2_test_sparksession') \
-#     .set
-
-# sc = SparkContext(conf = conf)
-# sqlContext = SQLContext(sc)
-
-# # print('spark.master :',   conf.get('spark.master'))
-# # print('spark.app.name :', conf.get('spark.app.name'))
-# # print('spark.home :',     conf.get('spark.home'))
-# # print('spark.home :',     conf.get('spark.driver.memory'))
-
-
-
-
-# # print(sc._conf.getAll())
-
-# for i in sc._conf.getAll():
-#     print(i)
-
-# sc.stop()
+sc.stop()
+'''

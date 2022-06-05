690f1fbc7ccf14cd214c669a618edddc2db8463b
박경태 <gyeongtae.park@catenoid.net>
2022-06-05 19:44:52 +0900

    spark-master ha 구성하는 방법

--- 3_spark-cluster-zookeeper/entrypoint.sh
+++ 3_spark-cluster-zookeeper/entrypoint.sh
@@ -5,13 +5,14 @@ sleep 1
 # 스파크 마스터 실행
 if [ ${SPARK_MODE} == "master" ]; then
     bash /home/spark/sbin/start-master.sh --properties-file /home/spark/ha.conf
+    # bash /home/spark/sbin/start-master.sh -p ${} --webui-port ${} --properties-file /home/spark/ha.conf
 fi
 
 sleep 1
 
 # 스파크 슬레이브 실행
 if [ ${SPARK_MODE} == "slave" ]; then
-    bash /home/spark/sbin/start-slave.sh spark://${SPARK_MASTER_HOST}:${SPARK_MASTER_PORT} -c ${SPARK_WORKER_CORES} -m ${SPARK_WORKER_MEMORY}
+    bash /home/spark/sbin/start-slave.sh spark://${SPARK_MASTER_HOST}:${SPARK_MASTER_PORT},${SPARK_MASTER_HOST_SUB}:${SPARK_MASTER_PORT_SUB} -c ${SPARK_WORKER_CORES} -m ${SPARK_WORKER_MEMORY}
 fi
 
 sleep 1
